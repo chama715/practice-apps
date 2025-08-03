73fcd48c1aae6a8e7a1350342ba068df885a4441
Naoto Takahashi <naoto0715charlie@gmail.com>
2025-08-03 14:01:18 +0900

    TimerApp

---
+++ TimerApp/TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by 高橋直斗 on 2025/08/03.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
