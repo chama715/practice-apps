727287964c96ce175416975a1f55a722f8d20674
Naoto Takahashi <naoto0715charlie@gmail.com>
2025-08-03 13:31:17 +0900

    おみくじ

--- OmikujirApp/OmikujirApp/ContentView.swift
+++ OmikujirApp/OmikujirApp/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var result = "今日の運勢は？？"
+    let fortunes = ["大吉","中吉","直吉","末吉","凶","大凶"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        // spacing40は、各要素間に40ptの余白をつけると言う意味
+        VStack(spacing: 40) {
+            Text(result)
+                .font(.largeTitle)
+                .padding()
+            
+            Button("おみくじを引く") {
+                let randomIndex = Int.random(in: 0..<fortunes.count)
+                result = fortunes[randomIndex]
+            }
+            .font(.title)
+            .padding()
+            .background(Color.pink.opacity(0.7))
+            .foregroundColor(.white)
+            .cornerRadius(10)
         }
         .padding()
     }
