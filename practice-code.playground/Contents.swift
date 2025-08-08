864bbc22906ca8538cd5c4d11c18fb86a16c55a1
Naoto Takahashi <naoto0715charlie@gmail.com>
2025-08-08 19:00:56 +0900

    practice

--- practice-code.playground/Contents.swift
+++ practice-code.playground/Contents.swift
@@ -1,7 +1,1 @@
-func createSelfIntro(name: String, hobby: String) -> String {
-    return "めろーーーーう！！おいどんの名前は\(name)でやーーーんすう！！！なんとなんと、趣味は\(hobby)でやんす！"
-}
-
-let intro = createSelfIntro(name: "ちゃま君", hobby: "アプリ開発")
-print(intro)
 
