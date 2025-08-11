80eabf627945e5c8df2854bb692bad26b175eded
Naoto Takahashi <naoto0715charlie@gmail.com>
2025-08-11 20:18:41 +0900

    関数

--- practice-code.playground/Contents.swift
+++ practice-code.playground/Contents.swift
@@ -1,1 +1,37 @@
+struct Cat {
+    
+    // プロパティ
+    var name: String
+    var age: Int
+    var favoriteToy: String
+    
+    // イニシャライザ
+    init(name: String, age: Int, favoriteToy: String = "ねずみのおもちゃ") {
+        self.name = name
+        self.age = age
+        self.favoriteToy = favoriteToy
+    }
+    
+    // メソッド
+    func introduce() -> String {
+        "にゃーん！僕は\(name)、\(age)歳。好きなおもちゃは\(favoriteToy)だよ！"
+    }
+    
+    // 年齢を1つ増やす
+    mutating func haveBirthday() {
+        age += 1
+    }
+}
+
+var chama = Cat(name: "ちゃま君", age: 32)
+
+print(chama.introduce())
+
+chama.haveBirthday()
+print(chama.introduce())
+
+let yoshio = Cat(name: "よしお", age: 3, favoriteToy: "ダンボール")
+print(yoshio.introduce())
+
+
 
